1033f3dee2e38f5fa4b02c60baacff0ebf059abc
Brilyan Hidayat <36091398+brilyanhidayat@users.noreply.github.com>
2024-07-30 11:48:30 +0700

    WF

--- mmw/index.php
+++ mmw/index.php
@@ -3,7 +3,7 @@
 
 <head>
    <meta charset="utf-8">
-   <title>Brulloft – Wedding planner HTML5 Template</title>
+   <title>MMW – Wedding planner HTML5 Template 2</title>
    <meta name="description" content="">
    <meta name="viewport" content="width=device-width, initial-scale=1">
 
